db8a805d64edca7336392626cff3a28267ba0f0e
robokrishan <haritkrishan176@gmail.com>
2024-11-25 14:17:17 +0100

    -

--- drive.py
+++ drive.py
@@ -27,7 +27,7 @@ while True:
             x1,y1,x2,y2 = line[0]
             cv2.line(gray,(x1,y1),(x2,y2),(0,255,0),2)
 
-    cv2.imshow("Camera Feed", gray)
+    cv2.imshow("Camera Feed", edges)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
