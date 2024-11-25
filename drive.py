b2d0f905a173faa39885810f6ca064f309086321
robokrishan <haritkrishan176@gmail.com>
2024-11-25 14:06:13 +0100

    added null condition

--- drive.py
+++ drive.py
@@ -22,9 +22,10 @@ while True:
     
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=200, maxLineGap=10)
 
-    for line in lines:
-        x1,y1,x2,y2 = line[0]
-        cv2.line(gray,(x1,y1),(x2,y2),(0,255,0),2)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line[0]
+            cv2.line(gray,(x1,y1),(x2,y2),(0,255,0),2)
 
     cv2.imshow("Camera Feed", gray)
 
