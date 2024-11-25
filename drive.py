6f3292e0502440bdd7aba9d925dbc34344c5704d
robokrishan <haritkrishan176@gmail.com>
2024-11-25 13:49:33 +0100

    fixed unpack

--- drive.py
+++ drive.py
@@ -21,17 +21,8 @@ while True:
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=200, maxLineGap=10)
 
     for line in lines:
-        rho, theta = line[0]
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-
-        cv2.line(gray, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        x1,y1,x2,y2 = line[0]
+        cv2.line(gray,(x1,y1),(x2,y2),(0,255,0),2)
 
     cv2.imshow("Camera Feed", gray)
 
