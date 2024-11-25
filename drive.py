9b47d25823341f7aba07c35ee322deb0ab04bdb4
robokrishan <haritkrishan176@gmail.com>
2024-11-25 13:40:38 +0100

    hough

--- drive.py
+++ drive.py
@@ -1,6 +1,7 @@
 from picamera2 import Picamera2
 from libcamera import ColorSpace, Transform
 import cv2
+import numpy as np
 
 picam2 = Picamera2()
 cam_config = picam2.create_preview_configuration(\
@@ -13,9 +14,26 @@ picam2.start()
 while True:
     frame = picam2.capture_array()
     
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("Camera Feed", rgb)
+    edges = cv2.canny(gray, 50, 200)
+    
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 200)
+
+    for line in lines:
+        rho, theta = line[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(gray, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imshow("Camera Feed", gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
